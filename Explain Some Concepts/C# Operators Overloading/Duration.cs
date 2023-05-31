4fca113d84e517771bad070a44d75d2a38d544db
Asim Qader <asem.adel00@gmail.com>
2023-05-31 18:15:02 +0300

    Add files via upload

---
+++ Explain Some Concepts/C# Operators Overloading/Duration.cs
@@ -0,0 +1,150 @@
+﻿
+namespace Duration
+{
+    class Duration
+    {
+        int Hours, Minutes, Seconds;
+
+        //default Ctor//
+        public Duration()
+        {
+                
+        }
+        //parametized Ctor takes three parameters//
+        public Duration(int h, int m, int s)
+        {
+            Hours = h;  
+            Minutes = m; 
+            Seconds = s;    
+        }
+        //parametized Ctor takes one parameter//
+        public Duration(int totalSec)
+        {
+            Hours = totalSec / 3600;
+            Minutes = (totalSec % 3600) / 60; 
+            Seconds = totalSec % 60; 
+        }
+
+     /*-----------------------------------------------*/
+
+        //overloading ToString() method//
+        public new void ToString()
+        {
+            Console.WriteLine($"Hours: {Hours}, Minutes: {Minutes}, Seconds: {Seconds}");
+        }
+
+     /*-----------------------------------------------*/
+
+        //operator + add two objects//
+        public static Duration operator +(Duration d1, Duration d2)
+        {
+            //Duration d3 = new Duration();    
+            //d3.Hours = d1.Hours + d1.Hours; 
+            //d3.Minutes = d1.Minutes + d1.Minutes;   
+            //d3.Seconds = d1.Seconds + d1.Seconds;   
+            //return d3;
+
+            //u can shorten the previous code, and add null propagation operator, as follow:
+            return new Duration(d1?.Hours ?? 0 + d2?.Hours ?? 0, d1?.Minutes?? 0 + d2?.Minutes ?? 0, d1?.Seconds ?? 0 + d2?.Seconds ?? 0);
+        }
+        //operator + add object to integer//
+        public static Duration operator +(Duration d1, int d2)
+        {
+            Duration d3 = new Duration(d2);
+            return new Duration(d1?.Hours + d3?.Hours ?? 0, d1?.Minutes + d3?.Minutes ?? 0, d1?.Seconds + d3?.Seconds ?? 0); //null propagation operator
+        }
+        //operator + add integer to object//
+        public static Duration operator +(int d1, Duration d2)
+        {
+            Duration d3 = new Duration(d1);
+            return new Duration(d2?.Hours + d3?.Hours ?? 0, d2?.Minutes + d3?.Minutes ?? 0, d2?.Seconds + d3?.Seconds ?? 0); //null propagation operator
+        }
+
+     /*-----------------------------------------------*/
+
+        //Increament and Decrement Operators (++, --)//
+        public static Duration operator ++(Duration d1)
+        {
+            int totalSec = (d1?.Hours * 60 * 60 ?? 0) + (d1?.Minutes * 60 ?? 0) + d1?.Seconds ?? 0; //convert ( Hours:Minutes:seconds ) to seconds only. And use null propagation operator in case the object parameters was null(set his all data -Hours,Minuts,Seconds- with zeros)
+
+            if(totalSec != 0) //means that d1 was not null, because if it is null then totalSec will equal 0
+                totalSec += 60;
+
+            Duration d2 = new(totalSec);
+            return d2; 
+        }
+        public static Duration operator --(Duration d1)
+        {
+            int totalSec = (d1?.Hours * 60 * 60 ?? 0) + (d1?.Minutes * 60 ?? 0) + d1?.Seconds ?? 0; //convert (Hours:Minutes:seconds) to seconds only. And use null propagation operator in case the object parameter was null.(set his all data -Hours,Minuts,Seconds- with zeros)
+
+            if (totalSec != 0)
+                totalSec -= 60;
+
+            Duration d2 = new(totalSec);
+            return d2;
+        }
+
+     /*-----------------------------------------------*/
+
+
+        //Logical Operators (<, > <=, >=)
+        public static bool operator >(Duration d1, Duration d2)
+        {
+            if(d1.Hours > d2.Hours)
+                return true;
+            if(d1.Hours == d2.Hours)
+            {
+                if(d1.Minutes > d2.Minutes) 
+                    return true;
+            }
+            if(d1.Hours == d2.Hours && d1.Minutes == d2.Minutes)
+            {
+                if (d1.Seconds > d2.Seconds)
+                    return true;
+            }
+
+            return false;   
+        }
+        public static bool operator <(Duration d1, Duration d2)
+        {
+            if (d1.Hours < d2.Hours)
+                return true;
+            if (d1.Hours == d2.Hours)
+            {
+                if (d1.Minutes < d2.Minutes)
+                    return true;
+            }
+            if (d1.Hours == d2.Hours && d1.Minutes == d2.Minutes)
+            {
+                if (d1.Seconds < d2.Seconds)
+                    return true;
+            }
+
+            return false;
+        }
+        public static bool operator <=(Duration d1, Duration d2)
+        {
+            if (d1.Hours <= d2.Hours && d1.Minutes <= d2.Minutes && d1.Seconds <= d2.Seconds)
+                return true;
+
+            return false;
+        }
+        public static bool operator >=(Duration d1, Duration d2)
+        {
+            if (d1.Hours >= d2.Hours && d1.Minutes >= d2.Minutes && d1.Seconds >= d2.Seconds)
+                return true;
+
+            return false;
+        }
+
+     /*-----------------------------------------------*/
+
+        //explicit castin with objects --> as: string str = (int)x;//
+        public static explicit operator DateTime(Duration d) 
+        {
+            DateTime dt = DateTime.Now;
+            return dt;
+        }
+
+    }
+}
---
+++ Explain Some Concepts/C# Operators Overloading/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Duration
+{
+    class Program
+    {
+        static void Main()
+        {
+            Duration d1 = new Duration(666);
+            d1.ToString();
+
+         //----------------------------------------------------//
+
+            Duration d2 = new Duration(1, 3, 20);
+            Duration d3 = new Duration(1, 59, 59);
+            Duration d4 = d2 + d3;
+            d4.ToString();
+
+         //----------------------------------------------------//
+
+            Duration d5 = default;
+            d4 = ++d5; //I made d5 = null(default), and because I handled it using null propagation in the metod, so it returns zeros, no exception thrown.
+            d4.ToString(); //Hours: 0, Minutes: 0, Seconds: 0
+
+            d4 = ++d3;
+            d4.ToString();
+            d4 = ++d3;
+            d4.ToString(); 
+            d3.ToString();  // d3 changed
+
+
+            d4 = --d5;
+            d4.ToString();
+
+         //----------------------------------------------------//
+
+            int seconds = 7800;
+            d4 = d3 + seconds;
+            d4.ToString();
+
+         //----------------------------------------------------//
+
+            Duration d6 = new Duration(2,10,30);
+            Duration d7 = new Duration(2, 10, 31);
+
+            Console.WriteLine(d7 > d6 ? "Yes" : "No");  //ternary operator
+            Console.WriteLine(d6 <= d5 ? "Yes" : "No");
+
+         //----------------------------------------------------//
+
+            Duration d8 = new(1, 45, 10);
+            DateTime dt = (DateTime)d8;
+            Console.WriteLine(dt);
+
+         //----------------------------------------------------//
+        }
+    }
+}
