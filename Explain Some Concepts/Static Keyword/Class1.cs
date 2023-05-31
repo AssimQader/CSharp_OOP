51a97c98636e20fd421376d3ac8f5c2ab0d63c3f
Asim Qader <asem.adel00@gmail.com>
2023-05-31 18:12:46 +0300

    Add files via upload

---
+++ Explain Some Concepts/Static Keyword/Class1.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Static_Keyword
+{
+    internal class Class1
+    {
+        int x; //non-static.
+
+
+
+        //Counter is a class attribute, not object attribute.
+        //called by the name of the class.
+        //shared among all objects from the class.
+        //allocated in Heap(initialized by zero) before the first usage of the class.
+        static int Counter;
+
+        //if u want to initialize a static attribute only once على مستوى الكلاس, do it inside the static constructor.
+        static double pi;
+
+
+
+
+
+        //static ctor cannot be preceded with access modifire(public, private,...).
+        //must be parameterless.
+        /*will called automaticlly and once مرة واحدة فقط  before creating any object, or calling any static method. 
+          لو استخدمت الكلاس بأي شكل سواء عملت أوبجكت أو استخدمت ستاتيك فانكشن فالستاتيك كونستراكتور هيتنفذ فوراً قبلهم  */
+
+        /*So in general we use static ctor to:
+              1- write inside it a code u want to exute only one time over the whole programe, 
+                 for example: when programe starts connect to the database and stay connected, do not reconnect it. 
+              2- initialize static attributes. */
+        static Class1()
+        {
+            Console.WriteLine("Static Ctor");
+            pi = 3.14;
+        }
+
+
+
+
+        //default ctor
+        public Class1()
+        {
+            Console.WriteLine("Default Ctor");
+            Counter++;
+        }
+
+
+
+
+        //static method: it is a calss behavior(cannot be accesed with object).
+        //deals only with a static members. 
+        //does not have access to "this" keyword, because "this" is a reference to the object that call the method, and static method cannot be called with objects.
+        public static void CounterZeroed()
+        {
+            Counter = 0;
+
+            //x = 0; //error: an object reference is required for non-static fields.
+        }
+    }
+}
---
+++ Explain Some Concepts/Static Keyword/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Static_Keyword
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Class1.CounterZeroed(); //static ctor called, but default ctor not.
+            /*this line causes the next بالترتييييب:
+            1. printout "Static Ctor" in console
+            2. initialize pi = 3.14 
+            3. excute CounterZeroed() method */
+        }
+    }
+}
