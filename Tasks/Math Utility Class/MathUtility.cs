489581405b35c829f78bf93f384c75af103e8ef1
Asim Qader <asem.adel00@gmail.com>
2023-05-31 16:45:37 +0300

    Add files via upload

---
+++ Tasks/Math Utility Class/MathUtility.cs
@@ -0,0 +1,23 @@
+﻿namespace MathClass
+{
+    //static class: use as utility... cannot create objects from it//
+    public static class MathUtility
+    {
+        public static int Add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public static int Substract(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+        public static int Multi(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+        public static int Divide(int n1, int n2)
+        {
+            return n2 == 0 ? -1 : n1 / n2;
+        }
+    }
+}
---
+++ Tasks/Math Utility Class/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MathClass
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 10, y =0;
+
+            Console.WriteLine(MathUtility.Add(x, y));
+            Console.WriteLine(MathUtility.Multi(x, y));
+            Console.WriteLine(MathUtility.Substract(x, y));
+
+            int result = MathUtility.Divide(x, y);
+            Console.WriteLine(result == -1 ? "Divid by zero is not right" : result);
+        }
+    }
+}
