cad370d8a1ee13e25b8cdd9490175678a0163502
Asim Qader <asem.adel00@gmail.com>
2023-05-31 18:16:57 +0300

    Add files via upload

---
+++ Explain Some Concepts/Singleton Design Pattern/Computer_NIC.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Sinelton_Explaination
+{
+    internal class Computer_NIC
+    {
+        static string Manufacture;
+        static string MacAddress;
+        static string Type;
+
+
+        //1- private object(default) ctor: this ctor is for الاستخدام الداخلي فقط, means that I can create objects only inside the class.
+        private Computer_NIC() { }
+
+
+
+        //2.1- private ctatic reference to object
+        static Computer_NIC cn;
+
+        /*2.2- private static ctor: put the initalization of the object here, beacuse as u know waht is inside static ctor is excuted only once over the program liftime.
+            and that is waht I want.. creating(initalizing) object for only one time. */
+        static Computer_NIC()
+        {
+            cn = new Computer_NIC();
+        }
+
+
+
+        //3- static method to set the data of ur NIC object(cn), then return it
+        public static Computer_NIC GetComputerNIC()
+        {
+            Manufacture = "DELL";
+            MacAddress = "CC-2F-71-C9-52-11";
+            Type = "Ethernet";
+
+            return cn;
+        }
+
+
+        //This shape of Singelton implementation is Thread-safe, because of static Ctor. 
+     //----------------------------------------------------------------------//
+
+
+        public void showData()
+        {
+            Console.WriteLine($"{Manufacture} , {MacAddress} , {Type}");
+        }
+    }
+}
---
+++ Explain Some Concepts/Singleton Design Pattern/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Sinelton_Explaination
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Computer_NIC cn1 = new Computer_NIC();
+            Computer_NIC cn2 = new Computer_NIC();  
+            //ur computer only have one NIC, it is not correct to create more than one NIC(object) to ur machine as in the previous line.
+            /*so we need to make the class Computer_NIC can create only one object(NIC) from it... so use "Singeltone" design pattern on this class. 
+               1- create a private default ctor: to prevent creating any objects from this class by the user or programmer in Main().
+               2- create ur own statitc object(NIC) inside the class itself. static is to make this attribute a class attribute, not object one. Then initialize it inside a stitc ctor.
+               3- create a static method that retutn this object.
+             */
+
+
+
+            //proof that only one object is created:
+            Computer_NIC cn1 = Computer_NIC.GetComputerNIC();
+            Console.WriteLine(cn1.GetHashCode()); //output: 27252167
+            cn1.showData(); //data
+
+            Computer_NIC cn2 = Computer_NIC.GetComputerNIC();
+            Console.WriteLine(cn1.GetHashCode()); //output: 27252167
+            cn1.showData();  //the same data
+
+
+          /* As u see: the identity number of cn1 is the same of cn2, this means that there is an only
+             one object and two refrences refere to it, not two objects :) */
+        }
+    }
+}
