5d4664677311fe3ad4ea62f65ac333f78647459d
Asim Qader <asem.adel00@gmail.com>
2023-05-31 16:49:31 +0300

    Add files via upload

---
+++ Tasks/NIC/NIC_Instance.cs
@@ -0,0 +1,38 @@
+﻿namespace NIC
+{
+    sealed class NIC_Instance
+    {
+        static string Manufacture;
+        static string MacAddress;
+        static string Type;
+
+
+     //  SINGELTON Design Pattern   //
+
+
+        //private ctor.
+        private NIC_Instance() { }
+
+
+        /*private static obj from the same class type.
+             Assignin it in the same line( = new NIC_Instance ) is the same if u do it in static ctor,
+             in IL code file the static ctor will be created by the compiler implicitlyضمنياً, and the ( = new NIC_Instance) statement is put inside it */
+        private static NIC_Instance Nic = new NIC_Instance(); 
+
+
+        //public static method returns the object.
+        public static NIC_Instance getNicSingeltone()
+        {
+            Manufacture = "DELL";
+            MacAddress = "00-15-5D-8F-FE-D6";
+            Type = "Ethernet";
+
+            return Nic;
+        }
+
+        public void showData()
+        {
+            Console.WriteLine($"{Manufacture} , {MacAddress} , {Type}");
+        }
+    }
+}
---
+++ Tasks/NIC/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace NIC
+{
+    class Program
+    {
+        static void Main()
+        {
+            NIC_Instance ni_1 = NIC_Instance.getNicSingeltone();
+            Console.WriteLine(ni_1.GetHashCode()); //output: 27252167
+            ni_1.showData();
+
+            NIC_Instance ni_2 = NIC_Instance.getNicSingeltone();
+            Console.WriteLine(ni_2.GetHashCode()); //output: 27252167
+            ni_2.showData();
+        }
+    }
+}
