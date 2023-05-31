42e308142607f453a47180cbc87031d78442be2d
Asim Qader <asem.adel00@gmail.com>
2023-05-31 17:20:14 +0300

    Add files via upload

---
+++ Tasks/Reverse String/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ReversString
+{
+    class Program
+    {
+        static string ReverseString(string s)
+        {
+            string[] str2 = s.Split(" ");  //split() returns array of string.
+
+            Array.Reverse(str2); //Reverse() method in Array class reversing the order of the array(words).
+
+            string str3 = string.Join(" ", str2); //join() method in string class turns the array of strings to one string.
+
+            return str3;
+        }
+        static void Main()
+        {
+            string str = Console.ReadLine();
+
+            Console.WriteLine(ReverseString(str));
+        }
+    }
+}
